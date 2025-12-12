f5c6100f126e4cb13b19d047cd6500eb0e896345
soggacat <soggacat@gmail.com>
2025-12-12 12:51:26 +0300

    Уууууууууууууууууууууууууууууууууууу

--- app/src/main/java/com/example/hd2wartracker/MainActivity.kt
+++ app/src/main/java/com/example/hd2wartracker/MainActivity.kt
@@ -22,22 +22,17 @@ class MainActivity : Activity() {
         totalMedalsText.text = "20000"
         progressBar.progress = 10
 
-        // Переход на moc_screen
+
         btnSwitch.setOnClickListener {
             val intent = Intent(this, moc_screen::class.java)
 
-            // Если нужно передать данные:
-            // intent.putExtra("totalMedals", "20000")
-            // intent.putExtra("progress", 65)
 
             startActivity(intent)
         }
 
-        // Переход на vc_screen
         btnVc.setOnClickListener {
             val intent = Intent(this, vc_screen::class.java)
 
-            // Передача данных (опционально)
             intent.putExtra("totalMedals", totalMedalsText.text.toString())
             intent.putExtra("progress", progressBar.progress)
 
@@ -48,7 +43,6 @@ class MainActivity : Activity() {
         btnUl.setOnClickListener {
             val intent = Intent(this, ul_screen::class.java)
 
-            // Передача данных (опционально)
             intent.putExtra("totalMedals", totalMedalsText.text.toString())
             intent.putExtra("progress", progressBar.progress)
 
